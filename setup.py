d39dee5897d2c8f74c266877291ffb46b61c3341
aiantorres <aiantorres@users.noreply.github.com>
2023-10-09 22:12:26 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting temperature in the United States by entering date and location',
+    author='aiantorres',
+    license='MIT',
+)
